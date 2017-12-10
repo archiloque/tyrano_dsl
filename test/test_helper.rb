28880ee0d4c4c820484f8110f9c6a72137bbf427
Julien Kirch <code@archiloque.net>
2017-12-10 22:48:09 +0100

    Coverage groups

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,10 @@
 require 'simplecov'
 SimpleCov.start do
-  add_filter "/test/"
+  add_filter '/test/'
+  add_group 'Elements', 'elements/'
+  add_group 'Elements writers', 'elements_writers/'
+  add_group 'File actions', 'file_actions/'
+  add_group 'Parsing words', 'parsing_words/'
+  add_group 'Writing words', 'writing_words/'
 end
 require 'minitest/autorun'
