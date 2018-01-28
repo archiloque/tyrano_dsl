9f648ea62fdd652f6250f236e34649ea5b583eb7
Julien Kirch <code@archiloque.net>
2018-01-29 00:03:30 +0100

    Clean code

--- lib/tyrano_dsl/export_dsl/main.rb
+++ lib/tyrano_dsl/export_dsl/main.rb
@@ -1,13 +1,33 @@
 require_relative 'export_dsl'
-require_relative 'writer'
+require_relative 'context'
 
 class TyranoDsl::ExportDsl::Main
+
+  def initialize
+    @logger = Logger.new(STDOUT)
+    @words = {}
+    TyranoDsl::Vocabulary.get_words_class('export_dsl/words') do |word, word_class|
+      @words[word] = word_class.new
+    end
+  end
 
   # @param [TyranoDsl::Elements::World] world
   # @param [Array<TyranoDsl::ParsedWord>] parsed_words
   # @return [Array]
+  # @raise [TyranoDsl::TyranoException]
   def run(world, parsed_words)
-    TyranoDsl::ExportDsl::Writer.new.run(world, parsed_words)
+    log {'Writing content'}
+    context = TyranoDsl::ExportDsl::Context.new(world)
+    parsed_words.each do |parsed_word|
+      action_word = @words[parsed_word.word]
+      action_word.run(
+          context,
+          parsed_word.word_location,
+          parsed_word.parameters
+      )
+    end
+    context.end_writing
+    context.file_actions
   end
 
   # @param [Array] file_actions
@@ -19,4 +39,10 @@ class TyranoDsl::ExportDsl::Main
     end
   end
 
+  private
+
+  def log
+    @logger.info(self.class) {yield}
+  end
+  
 end
